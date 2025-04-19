038ab06113cfc1c7fa17d876b4e1c396a848154b
t0183mq <prasanthreddy.sadam@stellantis.com>
2025-04-19 11:54:46 +0530

    RoboShop Project

--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,18 @@
 if [ -z "${MYSQL_ROOT_PASSWORD}" -a -z "$1" ]; then
-  echo input password is missing
-  exit 1
+  echo Input password is missing
+  exit
 fi
 
 if [ -n "${MYSQL_ROOT_PASSWORD}" ]; then
-  PASSWORD=${MYSQL_ROOT_PASSWORD}
+  PASSWORD=$MYSQL_ROOT_PASSWORD
 fi
 
-if [ -n "$1" ]; then
+if [ -n "$1" ];then
   PASSWORD=$1
 fi
 
 dnf install mysql-server -y
-
 systemctl enable mysqld
 systemctl start mysqld
+
+mysql_secure_installation --set-root-pass $PASSWORD
