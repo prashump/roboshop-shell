e7117262c4a28cb9c71a908205f56c7a8406a97c
t0183mq <prasanthreddy.sadam@stellantis.com>
2025-04-19 10:48:20 +0530

    RoboShop Project

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,10 @@
-
 component_name=catalogue
 source common.sh
 
 nodejs
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${pwd}/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
-mongosh --host mongodb-dev.prashumps.online </app/db/master-data.js
+mongosh --host mongodb-dev.rdevopsb84.online </app/db/master-data.js
 
 systemd_setup
