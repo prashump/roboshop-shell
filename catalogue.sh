38272619581f3416561c7989f3c6e2a3dd6d98c4
t0183mq <prasanthreddy.sadam@stellantis.com>
2025-04-19 10:46:23 +0530

    RoboShop Project

--- catalogue.sh
+++ catalogue.sh
@@ -4,7 +4,7 @@ source common.sh
 
 nodejs
 
-cp "$(pwd)"/mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.prashumps.online </app/db/master-data.js
 
